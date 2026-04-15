b03e0e0fa903e30712abed7c4bd27e564242cfbc
ArthurAraujoKk <arthuraraujo123ae@gmail.com>
2026-04-15 17:02:48 -0300

    Add files via upload

---
+++ Desafio.py
@@ -0,0 +1,8 @@
+lista_a = ["Doninha", "Lagarto", "Cobra"]
+print(f"Lista Veterinária: {lista_a}")
+
+print(f"Primeiro Animal: {lista_a[0]}")
+print(f"Último Animal: {lista_a[-1]}")
+print("Tatu adicionado na lista!")
+lista_a.append("Tatu")
+print(f"Lista Atualizada: {lista_a}")
